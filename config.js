dfc88fc8a5b6e04df583f358df0a01a916f19353
Ray <igalaxyray@gmail.com>
2024-04-18 20:48:52 +0100

    see last commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    filters: {
+      price_min: 0,
+      price_max: 1327,
+      wear_max: 0.38,
+      is_commodity: false,
+    },
+    recommendedPrice: 0, // Buying below +15 empire
+    buffTarget: 90, // Buying below 93% buff
+  };
